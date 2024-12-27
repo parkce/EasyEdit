7cf5ba6ada8abf22258b3535de182c36138c597d
parkce <parkce3@gmail.com>
2024-12-28 00:11:27 +0900

    update: add glob to get filename list from specific directory

--- do_core.py
+++ do_core.py
@@ -1,10 +1,13 @@
 from easyeditor import MultimodalTrainer, CoRECaptionDataset, CoREMultimodalTrainingHparams
-
+import glob
+from pathlib import Path
 
 training_hparams = CoREMultimodalTrainingHparams.from_hparams('./hparams/TRAINING/CoRE/blip2.yaml')
 
-train_ds = CoRECaptionDataset('/kilab/data/editing-data/caption/core_train.json', config=training_hparams)
-eval_ds = CoRECaptionDataset('/kilab/data/editing-data/caption/core_eval.json', config=training_hparams)
+directory_path = Path('/path/to/your/directory')
+# ann_paths, config
+train_ds = CoRECaptionDataset(glob.glob(directory_path / 'train/*json'), config=training_hparams)
+eval_ds = CoRECaptionDataset(glob.glob(directory_path / 'test/*json'), config=training_hparams)
 
 trainer = MultimodalTrainer(config=training_hparams, train_set=train_ds,val_set=eval_ds)
 trainer.run()
