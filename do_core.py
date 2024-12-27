b2d5ce6ef2db5d856d3786541a97a08b5c9c8ad7
parkce <parkce3@gmail.com>
2024-12-27 21:11:05 +0900

    update

--- do_core.py
+++ do_core.py
@@ -1,10 +1,10 @@
-from easyeditor import MultimodalTrainer, CaptionDataset, CoREMultimodalTrainingHparams
+from easyeditor import MultimodalTrainer, CoRECaptionDataset, CoREMultimodalTrainingHparams
 
 
 training_hparams = CoREMultimodalTrainingHparams.from_hparams('./hparams/TRAINING/CoRE/blip2.yaml')
 
-train_ds = CaptionDataset('/kilab/data/editing-data/caption/core_train.json', config=training_hparams)
-eval_ds = CaptionDataset('/kilab/data/editing-data/caption/core_eval.json', config=training_hparams)
+train_ds = CoRECaptionDataset('/kilab/data/editing-data/caption/core_train.json', config=training_hparams)
+eval_ds = CoRECaptionDataset('/kilab/data/editing-data/caption/core_eval.json', config=training_hparams)
 
 trainer = MultimodalTrainer(config=training_hparams, train_set=train_ds,val_set=eval_ds)
 trainer.run()
